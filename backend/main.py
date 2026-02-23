184aa42f09711bdf12c96ee2fbf4296be9c0b916
Luominai <kevinthepear@gmail.com>
2026-02-23 13:36:51 -0500

    email/password auth working

--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,9 @@ cred = credentials.Certificate(os.environ["PATH_TO_FIREBASE_ADMIN_KEY"])
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://ear-training-8f082.firebaseio.com'})
 db = firestore.client()
 doc = db.collection("users").document("pU1z2BwT9l0hO1p6R34a").get()
-if (doc.exists) :
-    print(doc.to_dict())
+
+if (doc.exists): # type: ignore
+    print(doc.to_dict()) # type: ignore
 
 app = FastAPI()
 
---
+++ frontend/app/(tabs)/login.tsx
@@ -0,0 +1,91 @@
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Button, TextInput } from "react-native";
+import { auth } from "../../firebaseConfig";
+
+export default function LoginScreen() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+    const [successMessage, setSuccessMessage] = useState("")
+
+    function login() {
+        setErrorMessage("")
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                setSuccessMessage("Successfully logged in")
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setErrorMessage(errorMessage)
+            });
+    }
+
+    return (
+        <ThemedView style={{
+            justifyContent: "center",
+            flexDirection: "row"
+        }}>
+            <ThemedView style={{
+                padding: 8,
+                gap: 8,
+                minWidth: 200,
+                maxWidth: 600,
+                flexGrow: 1,
+                flexShrink: 1
+            }}>
+                <ThemedView>
+                    <ThemedText>Email</ThemedText>
+                    <TextInput 
+                    style={{
+                        borderWidth: 1,
+                        height: 40,
+                        paddingHorizontal: 8
+                    }}
+                    value={email}
+                    onChangeText={(newText) => setEmail(newText)}
+                    />
+                </ThemedView>
+
+                <ThemedView>
+                    <ThemedText>Password</ThemedText>
+                    <TextInput 
+                    style={{
+                        borderWidth: 1,
+                        height: 40,
+                        paddingHorizontal: 8
+                    }}
+                    value={password}
+                    onChangeText={(newText) => setPassword(newText)}
+                    secureTextEntry
+                    />
+                </ThemedView>
+
+                <ThemedText style={{
+                    color: "green",
+                }}>
+                    {successMessage}
+                </ThemedText>
+                <ThemedText style={{
+                    color: "red",
+                }}>
+                    {errorMessage}
+                </ThemedText>
+
+                <ThemedView style={{
+                    height: 40,
+                }}>
+                    <Button
+                    title="Login"
+                    onPress={login}
+                    />
+                </ThemedView>
+            </ThemedView>
+        </ThemedView>
+    )
+}
